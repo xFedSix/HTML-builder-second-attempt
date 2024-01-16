3ce010d7b35485a141a0f5dfbc2bd02c3c7dd0e6
Fiodar Siyanka <siyankafiodar@gmail.com>
2024-01-16 23:16:14 +0300

    feat: add 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,31 @@
+const readline = require('readline');
+const fs = require('fs');
+
+const readLine = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const filePath = './02-write-file/notes.txt';
+
+fs.writeFile(filePath, '', (err) => {
+  if (err) throw err;
+  console.log('File created!');
+  promptInput();
+});
+
+function promptInput() {
+  readLine.question('Enter your text: ', (input) => {
+    if (input === 'exit') {
+      console.log('Goodbye!');
+      readLine.close();
+      return;
+    }
+
+    fs.appendFile(filePath, input + '\n', (err) => {
+      if (err) throw err;
+      console.log('Your text successfully added!');
+      promptInput();
+    });
+  });
+}
